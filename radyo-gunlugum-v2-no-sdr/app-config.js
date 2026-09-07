d0e4c4fb394a64b1c0c614639811b85fcc62208b
35burakoz-create <35burakoz@gmail.com>
2026-09-07 16:28:45 +0300

    V3.8.4: bump direct legacy cleanup version

--- radyo-gunlugum-v2-no-sdr/app-config.js
+++ radyo-gunlugum-v2-no-sdr/app-config.js
@@ -1,11 +1,11 @@
 (()=>{
 const config=Object.freeze({
-  version:'3.8.3',
-  displayVersion:'V3.8.3',
-  codename:'Guide Modernization',
+  version:'3.8.4',
+  displayVersion:'V3.8.4',
+  codename:'Direct Legacy Cleanup',
   timezone:'Europe/Istanbul',
   locale:'tr-TR',
-  cacheVersion:'v383-guide-modernization-20260907-1',
+  cacheVersion:'v384-direct-legacy-cleanup-20260907-1',
   origin:Object.freeze({name:'Bozköy, Torbalı, İzmir',lat:38.151,lon:27.36})
 });
 globalThis.RADIO_APP_CONFIG=config;
