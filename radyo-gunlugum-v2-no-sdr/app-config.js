9e994e01c0a9986cde75ed13bfd5a564b8361cdb
35burakoz-create <35burakoz@gmail.com>
2026-09-07 13:30:57 +0300

    V3.7.4: bump Router & Event Migration version

--- radyo-gunlugum-v2-no-sdr/app-config.js
+++ radyo-gunlugum-v2-no-sdr/app-config.js
@@ -1,11 +1,11 @@
 (()=>{
 const config=Object.freeze({
-  version:'3.7.3',
-  displayVersion:'V3.7.3',
-  codename:'Visual Foundation',
+  version:'3.7.4',
+  displayVersion:'V3.7.4',
+  codename:'Router & Event Migration',
   timezone:'Europe/Istanbul',
   locale:'tr-TR',
-  cacheVersion:'v373-visual-foundation-20260907-1',
+  cacheVersion:'v374-router-event-20260907-1',
   origin:Object.freeze({name:'Bozköy, Torbalı, İzmir',lat:38.151,lon:27.36})
 });
 globalThis.RADIO_APP_CONFIG=config;
