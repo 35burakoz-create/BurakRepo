935d43251b754b1e6a514f7f3ebf0dee59346155
35burakoz-create <35burakoz@gmail.com>
2026-09-07 13:43:04 +0300

    V3.7.5: set wrapper cleanup version

--- radyo-gunlugum-v2-no-sdr/app-config.js
+++ radyo-gunlugum-v2-no-sdr/app-config.js
@@ -1,11 +1,11 @@
 (()=>{
 const config=Object.freeze({
-  version:'3.7.4',
-  displayVersion:'V3.7.4',
-  codename:'Router & Event Migration',
+  version:'3.7.5',
+  displayVersion:'V3.7.5',
+  codename:'Wrapper Cleanup & Event UI',
   timezone:'Europe/Istanbul',
   locale:'tr-TR',
-  cacheVersion:'v374-router-event-20260907-1',
+  cacheVersion:'v375-wrapper-cleanup-20260907-1',
   origin:Object.freeze({name:'Bozköy, Torbalı, İzmir',lat:38.151,lon:27.36})
 });
 globalThis.RADIO_APP_CONFIG=config;
