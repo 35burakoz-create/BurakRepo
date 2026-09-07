00ffe93e44d69d3add6c63940ede5e4bf3d3690d
35burakoz-create <35burakoz@gmail.com>
2026-09-07 14:41:31 +0300

    V3.7.9: bump collection and Atlas cleanup release

--- radyo-gunlugum-v2-no-sdr/app-config.js
+++ radyo-gunlugum-v2-no-sdr/app-config.js
@@ -1,11 +1,11 @@
 (()=>{
 const config=Object.freeze({
-  version:'3.7.8',
-  displayVersion:'V3.7.8',
-  codename:'Listening & Smart Services Cleanup',
+  version:'3.7.9',
+  displayVersion:'V3.7.9',
+  codename:'Collection & Atlas Services Cleanup',
   timezone:'Europe/Istanbul',
   locale:'tr-TR',
-  cacheVersion:'v378-listening-services-20260907-1',
+  cacheVersion:'v379-collection-atlas-20260907-1',
   origin:Object.freeze({name:'Bozköy, Torbalı, İzmir',lat:38.151,lon:27.36})
 });
 globalThis.RADIO_APP_CONFIG=config;
