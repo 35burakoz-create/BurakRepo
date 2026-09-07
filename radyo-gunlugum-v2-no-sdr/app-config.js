9b19b4b0618b379652b2efc701823c5f09b6ccd3
35burakoz-create <35burakoz@gmail.com>
2026-09-07 12:25:01 +0300

    V3.7 add single app configuration source

---
+++ radyo-gunlugum-v2-no-sdr/app-config.js
@@ -0,0 +1,16 @@
+(()=>{
+const config=Object.freeze({
+  version:'3.7.0',
+  displayVersion:'V3.7',
+  codename:'Foundation',
+  timezone:'Europe/Istanbul',
+  locale:'tr-TR',
+  cacheVersion:'v370-foundation-20260907-1',
+  origin:Object.freeze({name:'Bozköy, Torbalı, İzmir',lat:38.151,lon:27.36})
+});
+globalThis.RADIO_APP_CONFIG=config;
+if(typeof document!=='undefined'){
+  document.documentElement.dataset.appVersion=config.version;
+  document.title=`Radyo Günlüğüm ${config.displayVersion}`;
+}
+})();
