9630ba394014abb956ca452b940f0407b2c50233
35burakoz-create <35burakoz@gmail.com>
2026-09-07 12:41:10 +0300

    V3.7.1: bump foundation config and cache version

--- radyo-gunlugum-v2-no-sdr/app-config.js
+++ radyo-gunlugum-v2-no-sdr/app-config.js
@@ -1,11 +1,11 @@
 (()=>{
 const config=Object.freeze({
-  version:'3.7.0',
-  displayVersion:'V3.7',
+  version:'3.7.1',
+  displayVersion:'V3.7.1',
   codename:'Foundation',
   timezone:'Europe/Istanbul',
   locale:'tr-TR',
-  cacheVersion:'v370-foundation-20260907-1',
+  cacheVersion:'v371-foundation-20260907-2',
   origin:Object.freeze({name:'Bozköy, Torbalı, İzmir',lat:38.151,lon:27.36})
 });
 globalThis.RADIO_APP_CONFIG=config;
