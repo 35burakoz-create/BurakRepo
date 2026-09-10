4ef0bf3bff8098282a4f68e7853cb60dd681f7da
35burakoz-create <35burakoz@gmail.com>
2026-09-10 14:45:44 +0300

    Bump app to V3.9.0 for A26 schedules

--- radyo-gunlugum-v2-no-sdr/app-config.js
+++ radyo-gunlugum-v2-no-sdr/app-config.js
@@ -1,11 +1,11 @@
 (()=>{
 const config=Object.freeze({
-  version:'3.8.5',
-  displayVersion:'V3.8.5',
-  codename:'Core Boundary Cleanup',
+  version:'3.9.0',
+  displayVersion:'V3.9.0',
+  codename:'A26 Schedule Integration',
   timezone:'Europe/Istanbul',
   locale:'tr-TR',
-  cacheVersion:'v385-core-boundary-20260907-1',
+  cacheVersion:'v390-a26-schedules-20260910-1',
   origin:Object.freeze({name:'Bozköy, Torbalı, İzmir',lat:38.151,lon:27.36})
 });
 globalThis.RADIO_APP_CONFIG=config;
